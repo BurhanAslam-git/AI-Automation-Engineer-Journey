b51503ed47c71e47878a36a79139f1e1f025ebe3
Burhan Aslam <burhanaslam944@gmail.com>
2026-03-29 01:32:37 +0500

    Refactor input handling and exception management

--- Phase1-Foundation/Week01-Python/Day03.py
+++ Phase1-Foundation/Week01-Python/Day03.py
@@ -9,120 +9,120 @@
 # print("Hello, world) # SyntaxError: EOL while scanning string literal
 
 
-# x = int(input("What's x? " ))
-# print(f"x is {x}")
+x = int(input("What's x? " ))
+print(f"x is {x}")
 
 
 # Using try & except to handle exceptions
-# try:
-#     x = int(input("What's x? " ))
-#     print(f"x is {x}")
+try:
+    x = int(input("What's x? " ))
+    print(f"x is {x}")
 
-# except ValueError:
-#     print("x is NOT an integer")
+except ValueError:
+    print("x is NOT an integer")
 
 #Name error
-# try:
-#     print(f"Hello, {name}")
-# except NameError:
-#     print("name is not defined")
+try:
+    print(f"Hello, {name}")
+except NameError:
+    print("name is not defined")
 
 
-# try:
-#     x = int(input("What's x? " ))
-# except ValueError:
-#     print("x is NOT an integer")
-# else:
-#     print(f"x is {x}")
+try:
+    x = int(input("What's x? " ))
+except ValueError:
+    print("x is NOT an integer")
+else:
+    print(f"x is {x}")
 
 
-# while True:
-#     try:
-#         x = int(input("What's x? " ))
-#     except ValueError:
-#         print("x is NOT an integer")
-#     else:
-#         break
+while True:
+    try:
+        x = int(input("What's x? " ))
+    except ValueError:
+        print("x is NOT an integer")
+    else:
+        break
 
-# print(f"x is {x}")
+print(f"x is {x}")
 
 
-# while True:
-#     try:
-#         x = int(input("What's x? " ))
-#         break
-#     except ValueError:
-#         print("x is NOT an integer")
+while True:
+    try:
+        x = int(input("What's x? " ))
+        break
+    except ValueError:
+        print("x is NOT an integer")
 
-# print(f"x is {x}")
+print(f"x is {x}")
 
 
 
 # Now using functions to do the same thing
-# def main():
-#     x = get_int()
-#     print(f"x is {x}")
+def main():
+    x = get_int()
+    print(f"x is {x}")
 
-# def get_int():
-#     while True:
-#         try:
-#             x = int(input("What's x? " ))
-#         except ValueError:
-#             print("x is NOT an integer")
-#         else:
-#             break
+def get_int():
+    while True:
+        try:
+            x = int(input("What's x? " ))
+        except ValueError:
+            print("x is NOT an integer")
+        else:
+            break
 
-#     return x
+    return x
 
-# main()
+main()
 
 # More intuitive
-# def main():
-#     x = get_int()
-#     print(f"x is {x}")
+def main():
+    x = get_int()
+    print(f"x is {x}")
 
-# def get_int():
-#     while True:
-#         try:
-#             x = int(input("What's x? " ))
-#         except ValueError:
-#             print("x is NOT an integer")
-#         else:
-#             return x
+def get_int():
+    while True:
+        try:
+            x = int(input("What's x? " ))
+        except ValueError:
+            print("x is NOT an integer")
+        else:
+            return x
 
-# main()
+main()
 
 
 #More compact
-# def main():
-#     x = get_int()
-#     print(f"x is {x}")
+def main():
+    x = get_int()
+    print(f"x is {x}")
 
-# def get_int():
-#     while True:
-#         try:
-#             return int(input("What's x? " ))
+def get_int():
+    while True:
+        try:
+            return int(input("What's x? " ))
 
-#         except ValueError:
-#             print("x is NOT an integer")
+        except ValueError:
+            print("x is NOT an integer")
     
 
-# main()
+main()
 
 
 #If we want we can pass also
-# def main():
-#     x = get_int()
-#     print(f"x is {x}")
+def main():
+    x = get_int()
+    print(f"x is {x}")
 
-# def get_int():
-#     while True:
-#         try: 
-#            return int(input("What's x? "))
-#         except ValueError:
-#             pass
+def get_int():
+    while True:
+        try: 
+           return int(input("What's x? "))
+        except ValueError:
+            pass
 
-# main()
+main()
 
 
 def main():
